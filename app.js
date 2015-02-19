cc3ca8c7e690a468ad9312e3fde607d8fb71e1d5
Emma Gardner <emmagardner@outlook.com>
2015-02-18 19:13:08 -0500

    2 word cities are now searchable in the home page

--- app.js
+++ app.js
@@ -16,9 +16,31 @@ weatherApp.filter('capitalize', function(){
   return  function(input){
     var words = input.split(' ');
     var result = [];
-    words.forEach(function(word){
-      result.push(word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
-    });
+    if (words.length > 1){
+      words.forEach(function(word){
+        result.push(word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+      });
+      return result.join('_');
+    } else {
+      return result.join(' ');
+    }
+  };
+});
+
+weatherApp.filter('unSnakecase', function(){
+  return function(input){
+    var result = [];
+    if (input.indexOf('_') !== -1){
+      var words = input.split('_');
+      words.forEach(function(word){
+        result.push(word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+      });
+    } else {
+      var otherwords = input.split(' ');
+      otherwords.forEach(function(word){
+        result.push(word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+      });
+    }
     return result.join(' ');
   };
 });
@@ -40,8 +62,8 @@ weatherApp.controller('homeController', ['$scope', '$filter', '$http', 'location
     locationService.state = $filter('uppercase')($scope.state);
   });
 
-  $scope.capitalizeCity = function(){
-    return $filter('capitalize')($scope.city);
+  $scope.unSnakeCity = function(){
+    return $filter('unSnakecase')($scope.city);
   };
   $scope.uppercaseState = function(){
     return $filter('uppercase')($scope.state);
@@ -51,6 +73,10 @@ weatherApp.controller('homeController', ['$scope', '$filter', '$http', 'location
 weatherApp.controller('todayController', ['$scope', '$filter', '$http', 'locationService', function($scope, $filter, $http, locationService){
   $scope.city= locationService.city;
   $scope.state= locationService.state;
+
+  $scope.unSnakeCity = function(){
+    return $filter('unSnakecase')($scope.city);
+  };
 
   var baseURL = "http://api.wunderground.com/api/";
 
@@ -62,6 +88,14 @@ weatherApp.controller('todayController', ['$scope', '$filter', '$http', 'locatio
           $scope.weather = data.current_observation.weather;
           $scope.temperature = data.current_observation.temp_f;
           $scope.wind = data.current_observation.wind_string;
+          console.log(data);
+        });
+      $http.get(baseURL + key + '/forecast/q/' + $scope.state + '/' + $scope.city + '.json')
+        .success(function(data){
+          var forecast = data.forecast.simpleforecast.forecastday[0];
+          $scope.hi = forecast.high.fahrenheit;
+          $scope.lo = forecast.low.fahrenheit;
+          $scope.forecast = data.forecast.txt_forecast.forecastday[0].fcttext;
           console.log(data);
         });
     });
